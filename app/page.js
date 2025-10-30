300d4589c36bd2d058b73b3844a7a2000a4da968
Emmy-delight <165961039+Emmy-delight@users.noreply.github.com>
2025-10-30 12:07:15 +0100

    created the footer and login page

--- app/page.js
+++ app/page.js
@@ -1,5 +1,45 @@
-export default function Home() {
+import Image from "next/image";
+import Link from "next/link";
+
+export default function HomePage () {
   return (
-       <main>Welcome to My Landing page</main>
-  );
+       <main>
+         <div className="bg-[url(/mybg.png)] h-screen bg-right bg-no-repeat bg-indigo-500">
+            <h1>Welcome to Fastcash</h1>
+         </div>
+         <div className="mt-10 grid grid-cols-4">
+            <Link href="https://www.earlycode.net"> <div className="w-[300px] h-[300px] rounded-lg shadow-md ">
+              <Image
+              src="/mybg.png"
+              alt="my-image"
+              width={300}
+              height={300}/>
+            </div>
+           </Link>
+            <div className="w-[300px] h-[300px] rounded-lg shadow-md ">
+              <Image
+              src="/mybg.png"
+              alt="my-image"
+              width={300}
+              height={300}/>
+           </div>
+            <div className="w-[300px] h-[300px] rounded-lg shadow-md ">
+              <Image
+              src="/mybg.png"
+              alt="my-image"
+              width={300}
+              height={300}/>
+           </div>
+            <div className="w-[300px] h-[300px] rounded-lg shadow-md ">
+              <Image
+              src="/mybg.png"
+              alt="my-image"
+              width={300}
+              height={300}/>
+           </div>
+         </div>
+       </main>
+  )
+
 }
+//authentication, authorization, 
---
+++ components/Sidebar.js
@@ -0,0 +1,5 @@
+export function Sidebar () {
+    return (
+        <main>This is our Sidebar</main>
+    )
+} 
